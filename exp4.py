8fc8727484b8b50ae581abf4188e8548277dad46
zhiyuan <86562713@qq.com>
2019-12-25 16:42:18 +0800

    Print training acc.

--- exp4.py
+++ exp4.py
@@ -9,7 +9,7 @@ x_train = x_train/255.0
 x_test = x_test/255.0
 
 # Define the network
-Network = ['MLP', 'LeNet'][1]
+Network = ['MLP', 'LeNet'][0]
 if Network=='MLP':
     config = {'EpochNum': 100, 'BatchSize': 50, 'InputDim': 784, 'OutputDim': 10, 'LayerNum': 3, 'HiddenNum': [1000,500,100], 'LearningRate': 0.01, 'lambda': 0.1}
     model = MLP(config)
@@ -25,6 +25,7 @@ for e in range(config['EpochNum']):
     y_train = y_train[perm]
     counter = 0
     loss = []
+    accuracy_train = []
     while counter + config['BatchSize'] <= x_train.shape[0]:
         if Network=='MLP':
             x_batch = x_train[counter:counter+config['BatchSize']].reshape([config['BatchSize'],-1])
@@ -36,13 +37,14 @@ for e in range(config['EpochNum']):
             feed = {model.input.name: x_batch, model.label.name: y_batch}
         feed.update(model.weight_value)
         loss.append(model.loss.eval(feed))
+        accuracy_train.append(model.accuracy.eval(feed))
         gradient = {k:v.back(model.loss,feed) for k,v in model.weight.items()}
         model.weight_value.update({
             k:model.weight_value[k]-config['LearningRate']*gradient[k] for k in model.weight.keys()})
         counter = counter + config['BatchSize']
     
     counter = 0
-    accuracy = []
+    accuracy_test = []
     while counter + config['BatchSize'] <= x_test.shape[0]:
         if Network=='MLP':
             x_batch = x_test[counter:counter+config['BatchSize']].reshape([config['BatchSize'],-1])
@@ -53,10 +55,10 @@ for e in range(config['EpochNum']):
             y_batch = one_hot(y_test[counter:counter+config['BatchSize']], config['OutputDim'])
             feed = {model.input.name: x_batch, model.label.name: y_batch}
         feed.update(model.weight_value)
-        accuracy.append(model.accuracy.eval(feed))
+        accuracy_test.append(model.accuracy.eval(feed))
         counter = counter + config['BatchSize']
 
-    print('Epoch ',e,' Done, Loss: ',round(np.mean(loss),4),', Test Accuracy: ',round(np.mean(accuracy),4))
+    print('Epoch ',e,' Done, Loss: ',round(np.mean(loss),4),', Train Acc: ',round(np.mean(accuracy_train),4),', Test Acc: ',round(np.mean(accuracy_test),4))
 
 
 
